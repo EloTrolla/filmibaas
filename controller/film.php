c44e46242f861cfd13780c4d6f86441e5b5a43aa
elotrolla <elo.trolla@khk.ee>
2015-11-18 08:51:32 +0200

    Make the controllers accept an ID parameter instead of a static ID [Delivers #107800804]

--- controller/film.php
+++ controller/film.php
@@ -1,5 +1,5 @@
 <?php
-
+$film_id = !empty($_GET['id']) ? $_GET['id'] : 1;
 //Connect to database
 $db = mysqli_connect('127.0.0.1', 'root', '', 'filmibaas') or die(mysqli_error($db));
 mysqli_query($db, "SET NAMES 'utf8'");
@@ -8,15 +8,17 @@ mysqli_query($db, "SET NAMES 'utf8'");
 $q = mysqli_query($db, "SELECT *, film.name as name, country.name as country
                          FROM film
                           JOIN country on film.country_id = country.country_id
-                         WHERE film_id=1");
+                         WHERE film_id=$film_id");
 $film = mysqli_fetch_assoc($q);
+
+print_r($film);
 
 //Retrieve all relationships for the film from database
 $qrel = mysqli_query($db, "SELECT link_type.name as type, author.name as author
                             FROM l_author_film
                             JOIN author ON author.author_id = l_author_film.author_id
                             JOIN link_type ON link_type.type_id = l_author_film.type_id
-                            WHERE film_id=1");
+                            WHERE film_id=$film_id");
 while ($row = mysqli_fetch_assoc($qrel)) {
     $relationships[] = $row;
 }
@@ -26,7 +28,7 @@ $qgen = mysqli_query($db, "SELECT genre.name as genre
                             FROM l_film_genre
                             JOIN film ON film.film_id = l_film_genre.film_id
                             JOIN genre ON genre.genre_id = l_film_genre.genre_id
-                            WHERE film.film_id=1");
+                            WHERE film.film_id=$film_id");
 while ($row = mysqli_fetch_assoc($qgen)) {
     $genres[] = $row;
 }
