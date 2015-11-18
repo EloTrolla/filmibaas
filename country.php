0e7950eb4a805c4f1cb4e9480fe297cb8219b88f
Nicolás Tamargo <reosarevok@gmail.com>
2015-11-18 09:08:43 +0200

    Add a check in case there are no authors/films

--- country.php
+++ country.php
@@ -19,9 +19,13 @@ require 'controller/country.php';
         <div class="entity-list col-md-6">
     <h2>Authors</h2>
     <?php
-    foreach ($data['authors'] as $author) {
-        echo '<a href="author.php?id=' . $author['author_id'] . '">'
-            . $author['name'] . ' (' . $author['begin_date_year'] . '-' . $author['end_date_year'] . ')</a><br>';
+    if ($data['authors']) {
+        foreach ($data['authors'] as $author) {
+            echo '<a href="author.php?id=' . $author['author_id'] . '">'
+                . $author['name'] . ' (' . $author['begin_date_year'] . '-' . $author['end_date_year'] . ')</a><br>';
+        }
+    } else {
+        echo "<p>We couldn't find any authors from this country.</p>";
     }
     ?>
 </div>
@@ -29,9 +33,13 @@ require 'controller/country.php';
         <div class="entity-list col-md-6">
     <h2>Films</h2>
     <?php
-    foreach ($data['films'] as $film) {
-        echo '<a href="film.php?id=' . $film['film_id'] . '">'
-            . $film['name'] . ' (' . $film['year'] . ')</a><br>';
+    if ($data['films']) {
+        foreach ($data['films'] as $film) {
+            echo '<a href="film.php?id=' . $film['film_id'] . '">'
+                . $film['name'] . ' (' . $film['year'] . ')</a><br>';
+        }
+    } else {
+        echo "<p>We couldn't find any films from this country.</p>";
     }
     ?>
 </div>
