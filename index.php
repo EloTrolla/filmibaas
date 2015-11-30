8faf7ad51a564d8daa1f56c22a8d9d3fa1d48767
elotrolla <elo.trolla@khk.ee>
2015-11-30 14:04:34 +0200

    controller kaustas author.php ja film.php muudetud, country.php muutmata

--- index.php
+++ index.php
@@ -6,4 +6,5 @@
  * Time: 12:36
  */
 $page = empty($_GET['page']) ? 'home' : $_GET['page'];
+require "model/database.php";
 require "view/$page.php";
---
+++ model/database.php
@@ -0,0 +1,30 @@
+<?php
+//Connect to database
+$db = mysqli_connect('127.0.0.1', 'root', '', 'filmibaas') or die(mysqli_error($db));
+mysqli_query($db, "SET NAMES 'utf8'");
+
+/**
+ * Executes the SQL and returns the all the rows as a 2-dimensional array.
+ * @param $sql
+ * @return array
+ */
+function get_all($sql)
+{
+    global $db;
+    $result = array();
+    $query_result = mysqli_query($db, $sql) or exit(mysqli_error($db));
+    while ($row = mysqli_fetch_assoc($query_result)) {
+        $result[] = $row;
+    }
+
+    return $result;
+}
+
+function get_first($sql)
+{
+    global $db;
+    $query_result = mysqli_query($db, $sql) or exit(mysqli_error($db));
+    $result = mysqli_fetch_assoc($query_result);
+
+    return empty($result) ? array() : $result;
+}
